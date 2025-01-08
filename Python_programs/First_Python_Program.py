7cdd49e18bf4fd50ece739a9a7ab71a883ddccea
Aishwarya Mahaveer Tanga <122354643+Aishutanga@users.noreply.github.com>
2025-01-08 13:40:35 +0000

    Added project files

---
+++ Python_programs/First_Python_Program.py
@@ -0,0 +1,16 @@
+#This is a comment
+print("Hello, World!")
+
+print("Hello, Recruiter!") #This is a comment
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, How are you!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, I am Aishwarya Tanga!")
